663629be7b9bd32c474e0330e6dbf0a3eaaaac9f
AngelMCV <angelmcabrera01@gmail.com>
2019-05-31 16:07:20 -0600

    first commit

---
+++ prue3.py
@@ -0,0 +1,15 @@
+print("suma 1")
+print("resta 2")
+op = int(input("ingrese un numero: "))
+if (op==1):
+    num1 = int(input("ingrese un numero: "))
+    num2 = int(input("ingrese otro numero:"))
+    print("su respuesta es: ", num1+num2)
+elif(op==2):
+    num1 = int(input("ingrese un numero: "))
+    num2 = int(input("ingrese otro numero:"))
+    print("su respuesta es: ", num1-num2)
+    print("si 1")
+
+elif (op>=3):
+    print("error.")
